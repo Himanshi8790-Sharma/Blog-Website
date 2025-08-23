ba17ea2ec8f71d81e17f8af7886b97f55318f5de
Himanshi8790-Sharma <004himanshi@gmail.com>
2025-08-22 22:53:51 -0400

    Update Pagination.jsx

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,35 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-function Pagination({  onPageChange,currentPage,blog,pageSize}) {
+function Pagination({ onPageChange, currentPage, totalBlogs, pageSize }) {
+  const totalPages = Math.ceil(totalBlogs / pageSize);
 
-    const totalPages = Math.ceil(blog.length / pageSize);
-    
-    const renderPaginationLinks = ()=>{
-        return Array.from({length: totalPages}, (_, i)=>i+1).map((pageNumber)=>
-        <li className={pageNumber === currentPage ? "activePagination" : "" } key={pageNumber}>
-           <a href="#" onClick={()=> onPageChange(pageNumber)}> {pageNumber}</a>
-        </li>
-        )
-    }
+  const renderPaginationLinks = () => {
+    return Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
+      <li
+        className={pageNumber === currentPage ? 'activePagination' : ''}
+        key={pageNumber}
+      >
+        <a href="#" onClick={(e) => { e.preventDefault(); onPageChange(pageNumber); }}>
+          {pageNumber}
+        </a>
+      </li>
+    ));
+  };
 
   return (
-    <ul  className='pagination  flex-wrap gap-4 mx-[150px]'>
-        <li>
-            <button onClick={()=> onPageChange(currentPage-1)} disabled={currentPage===1}>
-                Previous
-            </button>
-        </li>
-        <div className='flex gap-1'>
-            {renderPaginationLinks()}
-        </div>
-        <li>
-            <button onClick={()=> onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
-                Next
-            </button>
-        </li>
+    <ul className='pagination flex-wrap gap-4 mx-[150px]'>
+      <li>
+        <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
+          Previous
+        </button>
+      </li>
+      <div className='flex gap-1'>
+        {renderPaginationLinks()}
+      </div>
+      <li>
+        <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </li>
     </ul>
-  )
+  );
 }
 
-export default Pagination
+export default Pagination;
